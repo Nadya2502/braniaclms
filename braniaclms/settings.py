6604877cdca8464cfc3110c595bc92ffb1127370
Nadya2502 <fornmer@yandex.ru>
2022-05-27 22:00:27 +0300

    lesson_3

--- braniaclms/settings.py
+++ braniaclms/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'mainapp',
+    'mainapp'
 ]
 
 MIDDLEWARE = [
@@ -65,6 +65,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'mainapp.context_processor.my_context_processor',
             ],
         },
     },
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,7 +1,9 @@
 from django.views.generic import TemplateView
-
+import json
 from django.http import HttpResponse
 from django.views.generic import TemplateView
+from datetime import datetime
+from django.conf import settings
 
 class IndexView(TemplateView):
     template_name = 'mainapp/index.html'
@@ -15,11 +17,48 @@ class DocSiteView(TemplateView):
 class ContactsView(TemplateView):
     template_name = 'mainapp/contacts.html'
 
+    def get_context_data(self, **kwargs):
+        context_data = super().get_context_data(**kwargs)
+        context_data['contacts']=[
+            {
+                'map':'https://yandex.ru/map-widget/v1/-/CCUAZHcrhA',
+                'city': 'Санкт‑Петербург',
+                'phone': '+7-999-11-11111',
+                'email': 'geeklab@spb.ru',
+                'adress': 'территория Петропавловская крепость, 3Ж',
+
+            },
+            {
+                'map': 'https://yandex.ru/map-widget/v1/-/CCUAZHX3xB',
+                'city': 'Казань',
+                'phone': '+7-999-22-22222',
+                'email': 'geeklab@kz.ru',
+                'adress': 'территория Кремль, 11, Казань, Республика Татарстан, Россия',
+
+            },
+            {
+                'map': 'https://yandex.ru/map-widget/v1/-/CCUAZHh9kD',
+                'city': 'Москва',
+                'phone': '+7-999-33-33333',
+                'email': 'geeklab@msk.ru',
+                'adress': 'Красная площадь, 7, Москва, Россия',
+
+            }
+
+        ]
+        return context_data
 class LoginView(TemplateView):
     template_name = 'mainapp/login.html'
 
 class NewsView(TemplateView):
     template_name = 'mainapp/news.html'
+
+    def get_context_data(self, **kwargs):
+        context_data = super().get_context_data(**kwargs)
+        with open(settings.BASE_DIR / 'news.json', encoding='utf-8') as news_file:
+            context_data['object_list'] = json.load(news_file)
+
+        return context_data
 
 
 #class MainPageView (TemplateView):

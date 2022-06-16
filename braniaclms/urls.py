9637ee22b43f62d8ce8e2c43ded06cdef3e7f51a
Nadya2502 <fornmer@yandex.ru>
2022-06-16 14:03:58 +0300

    lesson_7

--- braniaclms/urls.py
+++ braniaclms/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+import debug_toolbar
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import RedirectView
@@ -30,6 +31,8 @@ urlpatterns = [
 
 ]
 if settings.DEBUG:
+
+    urlpatterns.append(path("__debug__/", include(debug_toolbar.urls)))
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
